dc7117c4fad65c6f281a50d90b22a41664dc2b83
LesterLyu <lvds2000@gmail.com>
2017-11-07 19:34:59 -0500

    Implemented Login Token System.  - All requests require token set in header {x-access-token: my-toekn} or body/param {token: my-token} Except 'register' and 'authenticate'

--- webserver/app.js
+++ webserver/app.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var config = require('./config'); // get our config file
+let config = require('./config'); // get our config file
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let index = require('./routes/index');
+let users = require('./routes/users');
+let register = require('./routes/register');
+let authenticate = require('./routes/authenticate');
 
-var app = express();
+let app = express();
+
+let morgan      = require('morgan');
+let mongoose    = require('mongoose');
+
+let jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+mongoose.connect(config.database); // connect to database
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,30 +28,80 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ---------------------------------------------------------
+// authentication (no middleware necessary since this isnt authenticated)
+// ---------------------------------------------------------
 app.use('/', index);
+app.use('/register', register);
+app.use('/authenticate', authenticate);
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+app.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    let token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, config.superSecret, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
+});
+
+
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
+
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
