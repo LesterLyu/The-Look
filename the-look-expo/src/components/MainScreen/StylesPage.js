05e198e5f7574f29fc40d3911daaaeca1ca79581
yanxi1 <xi.yan@mail.utoronto.ca>
2017-11-14 22:11:46 -0500

    added likes page

--- the-look-expo/src/components/MainScreen/StylesPage.js
+++ the-look-expo/src/components/MainScreen/StylesPage.js
@@ -20,6 +20,11 @@ const card1 = require("../../imgs/1.jpeg");
 const card2 = require("../../imgs/2.jpeg");
 const card3 = require("../../imgs/3.jpeg");
 const card4 = require("../../imgs/4.jpeg");
+const card5 = require("../../imgs/corporate.jpg");
+const card6 = require("../../imgs/hiphop.jpg");
+const card7 = require("../../imgs/jock.jpg");
+const card8 = require("../../imgs/popstar.jpg");
+const card9 = require("../../imgs/throwback.jpg");
 
 const cards = [
     {
@@ -42,6 +47,31 @@ const cards = [
         name: "4",
         image: card4
     },
+    {
+        text: "5",
+        name: "5",
+        image: card5
+    },
+    {
+        text: "6",
+        name: "6",
+        image: card6
+    },
+    {
+        text: "7",
+        name: "7",
+        image: card7
+    },
+    {
+        text: "8",
+        name: "8",
+        image: card8
+    },
+    {
+        text: "9",
+        name: "9",
+        image: card9
+    },
 ];
 
 export default class LearnMe extends React.Component {
@@ -59,22 +89,22 @@ export default class LearnMe extends React.Component {
                         </Button>
                     </Left>
                     <Body>
-                    <Title>Learn Me</Title>
+                    <Title>Styles</Title>
                     </Body>
                     <Right />
                 </Header>
                 <View style={{flex: 1, padding: 10}}>
                     <DeckSwiper ref={(mr) => this._deckSwiper = mr}
                                 dataSource={cards}
-                                looping={false}
-                                renderEmpty={() =>
-                                    <View style={{alignSelf: 'center'}}>
-                                        <Button full rounded light
-                                                style={{ marginTop: 10 }}
-                                                onPress={() => this.props.navigation.navigate("Home")} >
-                                            <Text>Check out my styles!</Text>
-                                        </Button>
-                                    </View>}
+                                looping={true}
+                                // renderEmpty={() =>
+                                //     <View style={{alignSelf: 'center'}}>
+                                //         <Button full rounded light
+                                //                 style={{ marginTop: 10 }}
+                                //                 onPress={() => this.props.navigation.navigate("Home")} >
+                                //             <Text>Check out my styles!</Text>
+                                //         </Button>
+                                //     </View>}
                                 renderItem={item =>
                                     <Card style={{elevation: 2}}>
                                         <CardItem cardBody>
